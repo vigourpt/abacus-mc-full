4ecfab10b21f89d59ed826d899764c4f05d04d04
vigourpt <vigourpt@googlemail.com>
2026-04-30 18:12:14 +0100

    fix: remove duplicate ternary close )} (already in </>)} on previous line)

--- src/components/panels/AgentVisualizerPanel.tsx
+++ src/components/panels/AgentVisualizerPanel.tsx
@@ -1119,7 +1119,6 @@ return (
             <span className="text-xs text-gray-500 min-w-[40px] text-right">{Math.round(zoom * 100)}%</span>
           </div>
           </>)}
-        )}
       </div>
 
       {/* Main Content */}
